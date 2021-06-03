dce60484212fff9cc9c90eb32c1b4e93e96d4c0a
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-06-03 16:55:40 +0200

    create operate function

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(a, operator, b) {
+  if (typeof (operator != "string")) {
+    return;
+  }
+  switch (operator) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return substract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+  }
+}
