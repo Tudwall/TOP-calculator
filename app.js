5c912d924530733913c4abf08f62385f3efda126
Pierre Laffaille <pierre.laffaille@outlook.fr>
2021-06-09 16:23:13 +0200

    make a fresh start

--- app.js
+++ app.js
@@ -9,29 +9,21 @@ const display = document.querySelector("#display");
 
 let firstNum = "";
 let secondNum = "";
-let operator = "";
 display.textContent = "0";
 
 // Event listeners.
-nbBtns.forEach((nbBtn) => {
-  nbBtn.addEventListener("click", () => {
-    if (display.textContent === "0") display.textContent = "";
-    display.textContent += nbBtn.textContent;
-  });
-});
-
-opBtns.forEach((opBtn) => {
-  opBtn.addEventListener("click", () => {
-    firstNum = display.textContent;
-    console.log(firstNum);
-    display.textContent += opBtn.textContent;
-    operator = opBtn.textContent;
-  });
+nbBtns.forEach((btn) => {
+  btn.addEventListener("click", () => inputNumber(btn.textContent));
 });
 
 clearBtn.addEventListener("click", clear);
 
 // Functions.
+function inputNumber(number) {
+  if (display.textContent === "0") display.textContent = "";
+  display.textContent += number;
+}
+
 function clear() {
   firstNum = "";
   secondNum = "";
